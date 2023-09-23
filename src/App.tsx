9a0ce1e5ed50cc4d6512eeb4e6016308a9453755
daichi0918 <kayasugadaichi@icloud.com>
2023-09-23 15:40:13 +0900

    機能一通り完成

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const initTodos = [
+    {task: 'Todo1'},
+    {task: 'Todo2'}
+  ]
+  const [todos, setTodos] = useState(initTodos);
+  const [todo, setTodo] = useState('');
+  const [searchInput, setSearchInput] =useState('');
+
+  const handleTaskInputChange = (e: React.ChangeEvent<HTMLInputElement>) => setTodo(e.target.value);
+  const handleSearchInputChange = (e: React.ChangeEvent<HTMLInputElement>) => setSearchInput(e.target.value);
+
+  const handleTaskAdd = (input: string) => {
+    if(input === '') return;
+    setTodos((todos) => [...todos, {task: todo }]);
+  }
+  const handleRemoveTask = (index: number) => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  }
+
+  const showTodoList = () => {
+    return todos.filter((todo) => {
+      const regexp = new RegExp("^" + searchInput, "i");
+      return todo.task.match(regexp);
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Todo List</h1>
+      <section>
+        <h2>ADD TODO</h2>
+        <input 
+          value={todo} 
+          type="text" 
+          placeholder="New Todo" 
+          onChange={handleTaskInputChange} 
+          onKeyUp={e => {
+            if (e.key === 'Enter') {
+              handleTaskAdd(todo)
+            }
+          }}
+          />
+      </section>
+      <section>
+        <input 
+        value={searchInput}
+        type="text" 
+        placeholder="Search KeyWord" 
+        onChange={handleSearchInputChange}
+        />
+      </section>
+      <section>
+        <ul>
+          {showTodoList().map((todo, index) => (
+            <div>
+              <li key={index}>
+                {todo.task}
+                <button onClick={() => handleRemoveTask(index)}>X</button>
+              </li>
+            </div>
+          ))}
+        </ul>
+      </section>
     </div>
   );
 }
